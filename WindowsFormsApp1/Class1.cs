351e001a709752e875eb30afa84592ff83a0608a
niry <nir.yom-tov@hpe.com>
2017-10-02 13:25:27 +0300

    Add project files.

---
+++ WindowsFormsApp1/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+
+  
+    class NameAndPassword
+    {
+        public String name;
+        public String password;
+
+        public NameAndPassword(String name, String password)
+        {
+            this.name = name;
+            this.password = password;
+        }
+    }
+}
---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Login = new System.Windows.Forms.Button();
+            this.name = new System.Windows.Forms.Label();
+            this.userName = new System.Windows.Forms.TextBox();
+            this.password = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Login
+            // 
+            this.Login.Location = new System.Drawing.Point(32, 190);
+            this.Login.Name = "Login";
+            this.Login.Size = new System.Drawing.Size(117, 52);
+            this.Login.TabIndex = 0;
+            this.Login.Text = "Login";
+            this.Login.UseVisualStyleBackColor = true;
+            this.Login.Click += new System.EventHandler(this.Login_Click);
+            // 
+            // name
+            // 
+            this.name.AutoSize = true;
+            this.name.Location = new System.Drawing.Point(29, 31);
+            this.name.Name = "name";
+            this.name.Size = new System.Drawing.Size(33, 13);
+            this.name.TabIndex = 1;
+            this.name.Text = "name";
+            // 
+            // userName
+            // 
+            this.userName.Location = new System.Drawing.Point(103, 27);
+            this.userName.Name = "userName";
+            this.userName.Size = new System.Drawing.Size(219, 20);
+            this.userName.TabIndex = 2;
+            // 
+            // password
+            // 
+            this.password.Location = new System.Drawing.Point(103, 62);
+            this.password.Name = "password";
+            this.password.Size = new System.Drawing.Size(219, 20);
+            this.password.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(29, 66);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "password";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(546, 369);
+            this.Controls.Add(this.password);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.userName);
+            this.Controls.Add(this.name);
+            this.Controls.Add(this.Login);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Login;
+        private System.Windows.Forms.Label name;
+        private System.Windows.Forms.TextBox userName;
+        private System.Windows.Forms.TextBox password;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
